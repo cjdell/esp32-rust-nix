4ae7322a09dcfdb4e38f00b921c7d5284c7feda2
Chris Dell <cjdell@gmail.com>
2024-10-27 14:09:46 +0000

    First successful build for ESP32-S3

---
+++ flake.nix
@@ -0,0 +1,99 @@
+# cargo generate esp-rs/esp-idf-template cargo
+
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+      fhs = pkgs.buildFHSUserEnv {
+        name = "fhs-shell";
+        targetPkgs = pkgs: with pkgs; [
+          gcc
+
+          pkg-config
+          libclang.lib
+          gnumake
+          cmake
+          ninja
+
+          git
+          wget
+
+          rustup
+          espup
+          cargo-generate
+
+          espflash
+          python3
+          python3Packages.pip
+          python3Packages.virtualenv
+          ldproxy
+        ];
+
+        runScript = pkgs.writeScript "run.sh" ''
+          #! ${pkgs.stdenv.shell}
+          espup install
+          source ~/export-esp.sh
+          ${pkgs.stdenv.shell}
+        '';
+      };
+    in
+    {
+      devShells.${pkgs.system}.default = fhs.env;
+    };
+}
+
+# {
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+#     nixpkgs-esp-dev = {
+#       url = "github:mirrexagon/nixpkgs-esp-dev";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#       # inputs.flake-utils.follows = "flake-utils";
+#     };
+#     esp32 = {
+#       url = "github:svelterust/esp32";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#   };
+
+#   outputs = {
+#     self,
+#     nixpkgs,
+#     nixpkgs-esp-dev,
+#     esp32,
+#   }: let
+#     system = "x86_64-linux";
+#     pkgs = import nixpkgs {
+#         inherit system;
+#         overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
+#     };
+#     idf-rust = esp32.packages.${system}.esp32;
+#   in {
+#     devShells.${system}.default = pkgs.mkShell {
+#       buildInputs = [
+#         pkgs.esp-idf-full
+#         idf-rust
+#         pkgs.tio
+#         pkgs.openssl
+#       ];
+
+#       nativeBuildInputs = [ pkgs.pkg-config ];
+
+#       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+
+#       shellHook = ''
+#         export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
+#         export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+#       '';
+#     };
+#   };
+# }
