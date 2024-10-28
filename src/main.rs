576d5644a1503bd14312ef5343ce5e02d1fc95da
Chris Dell <cjdell@gmail.com>
2024-10-28 18:18:12 +0000

    Trying to get components to work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use esp_idf_sys::picotts_init;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
@@ -8,9 +9,17 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    unsafe {
+        picotts_init(0, Some(foo), 0);
+    }
+
     loop {
         log::info!("Hello, world!");
 
         sleep(Duration::from_secs(1));
     }
+}
+
+unsafe extern "C" fn foo(a: *mut i16, b: u32) {
+    //
 }
