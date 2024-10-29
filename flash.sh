012549bf18d650de3abeb6ae9496a9fef62e8702
Chris Dell <cjdell@gmail.com>
2024-10-29 19:37:26 +0000

    TTS working but sounds awful

---
+++ flash.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cargo espflash partition-table partitions.csv
+
+cargo espflash flash --partition-table partitions.csv -s 8mb
+
+# espflash write-bin 0x291000 components/esp-picotts/pico/lang/en-GB_ta.bin
+# espflash write-bin 0x331000 components/esp-picotts/pico/lang/en-GB_kh0_sg.bin
+
+cargo espmonitor /dev/ttyACM0
